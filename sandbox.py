b43628d10a0f9dbe636a16e7fae500763654f12f
Wojciech Mazurkiewicz <wojciechdk@gmail.com>
2021-04-15 22:57:15 +0200

    df by country loaded

--- sandbox.py
+++ sandbox.py
@@ -2,10 +2,10 @@ import fiona
 import geopandas as gpd
 
 
-data = gpd.read_file("resources/mastophaengt_armatur.gpkg")
+data = gpd.read_file("resources/data/mastophaengt_armatur.gpkg")
 data.head()  # P
 
 # No need to pass "layer='etc'" if there's only one layer
-with fiona.open('resources/mastophaengt_armatur.gpkg', layer='layer_of_interest') as layer:
+with fiona.open('resources/data/mastophaengt_armatur.gpkg', layer='layer_of_interest') as layer:
     for feature in layer:
         print(feature['geometry'])
